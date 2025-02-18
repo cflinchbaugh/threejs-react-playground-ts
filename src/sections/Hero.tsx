ee89d580d53397083546bb80e32134c4358315c4
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-02-18 17:08:15 -0500

    Init Hero section, add waving animation

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 gap-3 sm:px-10 px-5">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Chris <span className="waving-hand">👋</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
